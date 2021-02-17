ae8c76a546a6ce183d72d41b222e87508b97c632
Your Name <csanchez112598@gmail.com>
2021-02-17 17:02:34 -0500

    made connect 4 object oriented

--- ai-bot.js
+++ ai-bot.js
@@ -1,76 +1,74 @@
+// simulation board for alorithm testing and development
 const sim = [
-	[ 1, 1, 0, 1, 1, 1, 2 ],
-	[ 1, 1, 0, 2, 2, 2, 1 ],
-	[ 2, 1, 0, 1, 1, 1, 2 ],
-	[ 1, 2, 0, 2, 2, 2, 1 ],
-	[ 1, 2, 0, 2, 1, 2, 2 ],
+	[ 2, 0, 0, 0, 0, 0, 0 ],
+	[ 2, 0, 0, 0, 0, 0, 0 ],
+	[ 1, 0, 0, 0, 0, 0, 0 ],
+	[ 1, 0, 1, 1, 1, 0, 0 ],
+	[ 1, 0, 2, 2, 1, 2, 2 ],
 	[ 1, 2, 2, 1, 2, 2, 2 ]
 ];
-
-const dummyBoard = board;
-
-const bot = 1;
-const human = 2;
+// define playing board to board from script.js
+// const dummyBoard = game.board;
+const bot = 2;
+const human = 1;
 let count = 0;
 
-function aiTurn() {
+//This function is called when it's bot's turn to play
+function aiTurn(dummyBoard) {
+	// console.log(dummyBoard);
+	//firstit checks for any immediate winning spots for bot or human
 	const winningSpot = checkWin(bot, dummyBoard);
 	const losingSpot = checkWin(human, dummyBoard);
+	//second it checks for any moves that can give human a win in human's next turn. And removes those moves from possibilities
 	const firstMoves = makeBoards(bot, dummyBoard); //bot
-	removeFlaggedSpotsFromFM(firstMoves);
+	removeFlaggedSpotsFromFirstMoves(firstMoves);
 
-
+	//if theres a winning spot, move there
 	if (winningSpot.length > 0) {
 		count++;
+		console.log('winning Spot');
 		return moveHere(winningSpot[0]);
 	} else if (losingSpot.length > 0) {
+		//if there;s a losing spot, move there
 		count++;
+		console.log('losing Spot');
 		return moveHere(losingSpot[0]);
 	} else if (count < 1) {
+		// on the first turn place next to human's piece (this strategy is to avoid human gaininng leverage in the first horizontal moves)
 		count++;
 		let possibleMoves = checkPosMoves(dummyBoard);
 		for (let thisMove of possibleMoves) {
 			let y = thisMove[0];
 			let x = thisMove[1];
 			if (dummyBoard[y][x + 1] === human) {
+				console.log('first Spot');
+
+				return moveHere([ y, x ]);
+			}
+		}
+	} else {
+		//bot will now calculate up to 3 moves ahead and decide to move based on which immidiate position offers least amount of losing possibilities or more winning possibilites wihtin the next 3 moves.
+		const secondMoves = makeBoardsForFirstMoves(firstMoves); //human
+		const thirdMoves = makeBoardsForSecondMoves(secondMoves); //bot
+		const fourthMoves = makeBoardsForThirdMoves(thirdMoves); //human
+		const possibleWins2 = findPotentialWinningSpots2(fourthMoves, firstMoves, dummyBoard);
+		if (possibleWins2.length > 0) {
+			console.log('possiblewins2 Spot');
+			return moveHere(possibleWins2);
+		}
+		let possibleMoves = checkPosMoves(dummyBoard);
+		for (let thisMove of possibleMoves) {
+			let y = thisMove[0];
+			let x = thisMove[1];
+			if (dummyBoard[y][x + 1] === human) {
+				console.log('possiblewins Spot');
 				return moveHere([ y, x ]);
 			}
 		}
 		//	 possibleMoves=checkPosMoves(dummyBoard);
 		let rand = Math.floor(Math.random() * possibleMoves.length);
-
 		return moveHere(possibleMoves[rand]);
-	} else {
-		const secondMoves = makeBoardsForFM(firstMoves); //human
-		const possibleWins = findPotentialWinningSpots(secondMoves, firstMoves);
-	
-		const thirdMoves = makeBoardsForSM(secondMoves); //bot
-		const leastLoses = findLeastLosingSpot(thirdMoves, firstMoves);
-	
-	
-		if (leastLoses.length > 0) {
-			return moveHere(leastLoses);
-		} else if (possibleWins.length > 0) {
-			return moveHere(possibleWins[0]);
-		} 
-		 else {
-			const fourthMoves = makeBoardsForTM(thirdMoves); //human
-			const possibleWins2 = findPotentialWinningSpots2(fourthMoves, firstMoves);
-			 if (possibleWins.length > 0) {
-				return moveHere(possibleWins2);
-			 }
-			let possibleMoves = checkPosMoves(dummyBoard);
-			for (let thisMove of possibleMoves) {
-				let y = thisMove[0];
-				let x = thisMove[1];
-				if (dummyBoard[y][x + 1] === human) {
-					return moveHere([ y, x ]);
-				}
-			}
-			//	 possibleMoves=checkPosMoves(dummyBoard);
-			let rand = Math.floor(Math.random() * possibleMoves.length);
-			return moveHere(possibleMoves[rand]);
-		}
+		// }
 	}
 }
 
@@ -84,7 +82,7 @@ function findMoveFromBoard(board, currBoard) {
 	}
 }
 
-function removeFlaggedSpotsFromFM(firstMoves) {
+function removeFlaggedSpotsFromFirstMoves(firstMoves) {
 	// const flaggedSpots=[];
 	firstMoves.forEach((board, i) => {
 		if (checkWin(human, board).length > 0) {
@@ -94,7 +92,7 @@ function removeFlaggedSpotsFromFM(firstMoves) {
 	});
 }
 
-function findPotentialWinningSpots2(fourthMove, firstMoves) {
+function findPotentialWinningSpots2(fourthMove, firstMoves, dummyBoard) {
 	const thisArr2 = [];
 	let c2 = 0;
 	for (let thirdMove of fourthMove) {
@@ -118,46 +116,7 @@ function findPotentialWinningSpots2(fourthMove, firstMoves) {
 	const max = Math.max(...thisArr2);
 	// console.log(thisArr2,"<--thisArr, min-->",max);
 	// console.log(thisArr2.indexOf(max),"<--index. move--> ",findMoveFromBoard(firstMoves[thisArr2.indexOf(max)],dummyBoard));
-
 	return findMoveFromBoard(firstMoves[thisArr2.indexOf(max)], dummyBoard);
-}
-
-function findPotentialWinningSpots(secondMove, firstMoves) {
-	const potentialWinningBoards = [];
-	for (let firstMove of secondMove) {
-		for (let board of firstMove) {
-			const winningSpot = checkWin(bot, board);
-			if (winningSpot.length > 0) {
-				potentialWinningBoards.push(findMoveFromBoard(firstMoves[secondMove.indexOf(firstMove)], dummyBoard));
-				break;
-			}
-		}
-	}
-	return potentialWinningBoards;
-}
-
-function findLeastLosingSpot(thirdMove, firstMoves) {
-	let c = 0;
-	let thisArr = [];
-	for (let first of thirdMove) {
-		for (let second of first) {
-			for (let board of second) {
-				const winningSpot = checkWin(human, board);
-				if (winningSpot.length > 0) {
-					c++;
-				}
-			}
-		}
-		thisArr.push(c);
-		c = 0;
-	}
-	if (thisArr.length < 1) {
-		return [];
-	}
-	const min = Math.min(...thisArr);
-	console.log(thisArr, '<--thisArr, min-->', min);
-	// console.log(thisArr.indexOf(min),"<--index. move--> ",findMoveFromBoard(firstMoves[thisArr.indexOf(min)],dummyBoard));
-	return findMoveFromBoard(firstMoves[thisArr.indexOf(min)], dummyBoard);
 }
 
 function makeBoards(currPlayer, board) {
@@ -171,7 +130,7 @@ function makeBoards(currPlayer, board) {
 	return boards;
 }
 
-function makeBoardsForFM(firstMoves) {
+function makeBoardsForFirstMoves(firstMoves) {
 	const secondMoves = [];
 	for (let board of firstMoves) {
 		secondMoves.push(makeBoards(human, board));
@@ -179,7 +138,7 @@ function makeBoardsForFM(firstMoves) {
 	return secondMoves;
 }
 
-function makeBoardsForSM(secondMoves) {
+function makeBoardsForSecondMoves(secondMoves) {
 	const thirdMoves = [];
 	for (let firstMoves of secondMoves) {
 		const thirdMove = [];
@@ -191,7 +150,7 @@ function makeBoardsForSM(secondMoves) {
 	return thirdMoves;
 }
 
-function makeBoardsForTM(thirdMoves) {
+function makeBoardsForThirdMoves(thirdMoves) {
 	const fourthMoves = [];
 	for (let secondMoves of thirdMoves) {
 		const third3Moves = [];
@@ -207,36 +166,18 @@ function makeBoardsForTM(thirdMoves) {
 	return fourthMoves;
 }
 
-function moveHere(pos) {
-	console.log('bot move here', pos);
-	return pos;
-}
-
 function checkWin(currPlayer, board) {
 	const possibleMoves = checkPosMoves(board);
 	const wins = [];
+	//console.log(possibleMoves,currPlayer);
+
 	for (let thisMove of possibleMoves) {
 		const predBoard = JSON.parse(JSON.stringify(board));
 		let yMove = thisMove[0];
 		let xMove = thisMove[1];
 		predBoard[yMove][xMove] = currPlayer;
-
-		function _win(cells) {
-			return cells.every(
-				([ y, x ]) => y >= 0 && y < HEIGHT && x >= 0 && x < WIDTH && predBoard[y][x] === currPlayer
-			);
-		}
-		for (let y = 0; y < HEIGHT; y++) {
-			for (let x = 0; x < WIDTH; x++) {
-				var horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
-				var vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ];
-				var diagDR = [ [ y, x ], [ y + 1, x + 1 ], [ y + 2, x + 2 ], [ y + 3, x + 3 ] ];
-				var diagDL = [ [ y, x ], [ y + 1, x - 1 ], [ y + 2, x - 2 ], [ y + 3, x - 3 ] ];
-
-				if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
-					wins.push([ yMove, xMove ]);
-				}
-			}
+		if (game.checkForWin(currPlayer, predBoard) === true) {
+			wins.push([ yMove, xMove ]);
 		}
 	}
 	return wins;
@@ -245,12 +186,14 @@ function checkWin(currPlayer, board) {
 function checkPosMoves(board) {
 	const possibleMoves = [];
 	for (let x = 0; x < board[0].length; x++) {
-		for (let y = board.length - 1; y >= 0; y--) {
-			if (board[y][x] === 0) {
-				possibleMoves.push([ y, x ]);
-				break;
-			}
-		}
+		y = game.findSpotForCol(x, board);
+		if (y != null) possibleMoves.push([ y, x ]);
 	}
 	return possibleMoves;
 }
+
+function moveHere(pos) {
+	console.log('bot move here', pos);
+	return pos;
+}
+// aiTurn(sim);
---
+++ connect4-oo.js
@@ -0,0 +1,155 @@
+/** Connect Four
+ *
+ * Player 1 and 2 alternate turns. On each turn, a piece is dropped down a
+ * column until a player gets four-in-a-row (horiz, vert, or diag) or until
+ * board fills (tie)
+ */
+
+// const WIDTH = 7;
+// const HEIGHT = 6;
+// let currPlayer = 1; // active player: 1 or 2
+// let board = []; // array of rows, each row is array of cells  (board[y][x])
+
+class Game {
+	constructor(height = 6, width = 7, p1 = 1, p2 = 2) {
+		this.height = height;
+		this.width = width;
+		this.currPlayer = p1;
+		this.players = [ p1, p2 ];
+    // this.handleClickStart();
+	}
+
+	handleClickStart(player) {
+		this.player = player;
+		const start = document.getElementById('flex-box');
+		start.setAttribute('class', 'hidden');
+		this.makeBoard();
+		this.makeHtmlBoard();
+	}
+	makeBoard() {
+		this.board = [];
+		for (let i = 0; i < this.height; i++) {
+			let arr = [];
+			for (let j = 0; j < this.width; j++) {
+				arr.push(0);
+			}
+			this.board.push(arr);
+		}
+	}
+	makeHtmlBoard() {
+		const board1 = document.getElementById('board');
+		// make column tops (clickable area for adding a piece to that column)
+		const top = document.createElement('tr');
+		top.setAttribute('id', 'column-top');
+		this.moveHuman = this.moveHuman.bind(this); /////?????
+		top.addEventListener('click', this.moveHuman);
+
+		for (let x = 0; x < this.width; x++) {
+			const headCell = document.createElement('td');
+			headCell.setAttribute('id', x);
+			top.append(headCell);
+		}
+		board1.append(top);
+		// make main part of board
+		for (let y = 0; y < this.height; y++) {
+			const row = document.createElement('tr');
+
+			for (let x = 0; x < this.width; x++) {
+				const cell = document.createElement('td');
+				cell.setAttribute('id', `${y}-${x}`);
+				row.append(cell);
+			}
+			board1.append(row);
+		}
+	}
+
+	findSpotForCol(x,board) {
+		for (let y = this.height - 1; y >= 0; y--) {
+			if (!board[y][x]) {
+				return y;
+			}
+		}
+		return null;
+	}
+	placeInTable(y, x) {
+		const piece = document.createElement('div');
+		piece.classList.add('piece');
+		piece.classList.add(`p${this.currPlayer}`);
+		piece.style.top = -50 * (y + 2);
+
+		const spot = document.getElementById(`${y}-${x}`);
+		spot.append(piece);
+	}
+	endGame(msg) {
+    setTimeout(()=>alert(msg),300)
+	}
+
+	moveHuman(evt) {
+		// get x from ID of clicked cell
+		const x = +evt.target.id;
+
+		// get next spot in column (if none, ignore click)
+		const y = this.findSpotForCol(x,this.board);
+		if (y === null) {
+			return;
+		}
+
+		// place piece in board and add to HTML table
+		this.board[y][x] = this.currPlayer;
+		this.placeInTable(y, x);
+		this.evaluateMove();
+	}
+
+	evaluateMove() {
+		// check for win
+		if (this.checkForWin(this.currPlayer, this.board)) {
+			this.gameOver = true;
+			return this.endGame(`Player ${this.currPlayer} won!`);
+		}
+		// check for tie
+		if (this.board.every((row) => row.every((cell) => cell))) {
+			return this.endGame('Tie!');
+		}
+		// switch players
+		if (this.currPlayer === this.players[0]) {
+			this.currPlayer = this.players[1];
+			if (this.player) this.moveAI();
+		} else {
+			this.currPlayer = this.players[0];
+		}
+	}
+	moveAI() {
+		let move = aiTurn(this.board);
+		let y = move[0];
+		let x = move[1];
+
+		this.board[y][x] = this.currPlayer;
+    setTimeout(()=>{
+      this.placeInTable(y, x);
+      this.evaluateMove();}
+    ,0)
+		
+	}
+
+	checkForWin(player, board) {
+		const _win = (cells) =>
+			cells.every(([ y, x ]) => y >= 0 && y < this.height && x >= 0 && x < this.width && board[y][x] === player);
+
+		for (let y = 0; y < this.height; y++) {
+			for (let x = 0; x < this.width; x++) {
+				// get "check list" of 4 cells (starting here) for each of the different
+				// ways to win
+				const horiz = [ [ y, x ], [ y, x + 1 ], [ y, x + 2 ], [ y, x + 3 ] ];
+				const vert = [ [ y, x ], [ y + 1, x ], [ y + 2, x ], [ y + 3, x ] ];
+				const diagDR = [ [ y, x ], [ y + 1, x + 1 ], [ y + 2, x + 2 ], [ y + 3, x + 3 ] ];
+				const diagDL = [ [ y, x ], [ y + 1, x - 1 ], [ y + 2, x - 2 ], [ y + 3, x - 3 ] ];
+				// find winner (only checking each win-possibility as needed)
+				if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
+					return true;
+				}
+			}
+		}
+	}
+}
+
+let game = new Game();
